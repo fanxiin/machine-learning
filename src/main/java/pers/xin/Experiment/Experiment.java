39d466711ffaed7d4bd11b95f301ca333e9dd0eb
xin <fanxinchn@foxmail.com>
2017-04-27 00:54:39 +0800

    v0.12

--- src/main/java/pers/xin/Experiment/Experiment.java
+++ src/main/java/pers/xin/Experiment/Experiment.java
@@ -1,4 +1,4 @@
-package pers.xin.mian;
+package pers.xin.Experiment;
 
 import pers.xin.optimization.Optimizable;
 import swjtu.ml.filter.FSException;
@@ -23,6 +23,7 @@ public class Experiment implements Optimizable{
     private String classifierName;
     private double[][] interval;
     private HashMap<String,Double> featuresAUC=new HashMap<String, Double>();
+    private HashMap<Long,Double> AUCBuffer = new HashMap<Long, Double>();
 
     public Experiment(String classifierName, Instances data){
         this.classifierName =classifierName;
@@ -45,6 +46,10 @@ public class Experiment implements Optimizable{
      */
     public double fitness(double[] params){
         double AUC = 0;
+//        for (int i = 0; i < params.length; i++) {
+//            BigDecimal b = new BigDecimal(params[i]);
+//            params[i] = b.setScale(3,BigDecimal.ROUND_HALF_UP).doubleValue();
+//        }
         try {
             AUC = RSFSAIDTest(params[0],params[1],params[2]);
         } catch (FSException e) {
@@ -77,16 +82,20 @@ public class Experiment implements Optimizable{
      * @throws Exception 参数不合适时抛出FSException
      */
     public double RSFSAIDTest(double delta, double alpha, double beta) throws Exception {
-        RSFSAID rsfsaid = new RSFSAID(delta,alpha,beta);
-        FeatureSelection fs = new FeatureSelection(rsfsaid);
-        fs.setInputFormat(data);
-        String features = fs.selectFeature(data);
-        if(!featuresAUC.containsKey(features)){
-            Instances fs_data = Filter.useFilter(data,fs);
-            Classifier classifier = (Classifier) Class.forName(classifierName).newInstance();
-            featuresAUC.put(features,computeAUC(fs_data,classifier));
+        long paramsCode = Math.round(delta*100)*10000+Math.round(alpha*100)*100+Math.round(beta*100);
+        if(!AUCBuffer.containsKey(paramsCode)){
+            RSFSAID rsfsaid = new RSFSAID(delta,alpha,beta);
+            FeatureSelection fs = new FeatureSelection(rsfsaid);
+            fs.setInputFormat(data);
+            String features = fs.selectFeature(data);
+            if(!featuresAUC.containsKey(features)){
+                Instances fs_data = Filter.useFilter(data,fs);
+                Classifier classifier = (Classifier) Class.forName(classifierName).newInstance();
+                featuresAUC.put(features,computeAUC(fs_data,classifier));
+            }
+            AUCBuffer.put(paramsCode,featuresAUC.get(features));
         }
-        return featuresAUC.get(features);
+        return AUCBuffer.get(paramsCode);
     }
 
     /**
@@ -117,35 +126,29 @@ public class Experiment implements Optimizable{
      * @return
      * @throws Exception
      */
-    public String originalAnalyze() throws Exception {
+    public Summary originalAnalyze() throws Exception {
         Evaluation eval = new Evaluation(data);
         Classifier classifier = (Classifier) Class.forName(classifierName).newInstance();
         eval.crossValidateModel(classifier,data,10,new Random(1));
-        return "precision:"+eval.precision(positiveIndex)+
-                ", AUC："+eval.areaUnderROC(positiveIndex)+
-                ", fMeasure:"+eval.fMeasure(positiveIndex);
+        return new Summary(eval,positiveIndex);
     }
 
     /**
      * 获得分类器对RSFSAID算法特征选择后的训练集的数据训练得出的分类指标
-     * @param delta
-     * @param alpha
-     * @param beta
+     * @param params
      * @return
      * @throws Exception
      */
-    public String RSFSAIDAnalyze(double delta, double alpha, double beta) throws Exception {
-        RSFSAID rsfsaid = new RSFSAID(delta,alpha,beta);
+    public Summary RSFSAIDAnalyze(double[] params) throws Exception {
+        RSFSAID rsfsaid = new RSFSAID(params[0],params[1],params[2]);
         FeatureSelection fs = new FeatureSelection(rsfsaid);
         fs.setInputFormat(data);
+        String reduction = fs.selectFeature(data);
         Instances fs_data = Filter.useFilter(data,fs);
         Evaluation eval = new Evaluation(fs_data);
         Classifier classifier = (Classifier) Class.forName(classifierName).newInstance();
         eval.crossValidateModel(classifier,fs_data,10,new Random(1));
-        return "选择的属性: " + rsfsaid.getSelectedAttributesString()+"\n"+
-                "precision:"+eval.precision(positiveIndex)+
-                ", AUC："+eval.areaUnderROC(positiveIndex)+
-                ", fMeasure:"+eval.fMeasure(positiveIndex);
+        return new Summary(params, reduction, eval, positiveIndex);
     }
 
     /**
